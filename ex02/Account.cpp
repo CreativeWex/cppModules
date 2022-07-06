7bbfb56bfcb219fc42301872430f44cfa28ffba8
Jarod Nidorino <jnidorin@pr-d1.kzn.21-school.ru>
2022-07-06 17:55:20 +0300

    started 02

---
+++ ex02/Account.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Account.hpp"
+
+Account::Account(int initial_deposit)
+{
+    //TODO:
+}
+
+Account::~Account()
+{
+    //TODO:
+}
+
+void Account::makeDeposit(int deposite)
+{
+    //TODO:
+}
+
+bool Account::makeWithdrawal( int withdrawal ) const
+{
+     //TODO:
+}
+
+int Account::checkAmount( void ) const;
+
