754ae22115eea61aa9de709d53f692752b40f6f4
Jarod Nidorino <jnidorin@un-i4.kzn.21-school.ru>
2022-07-08 17:40:39 +0300

    03 доделать

---
+++ cppModule01/!ex03/HumanA.hpp
@@ -0,0 +1,19 @@
+#ifndef HUMANA_HPP
+#define HUMANA_HPP
+
+#include "Weapon.hpp"
+
+class HumanA
+{
+    public:
+        HumanA(std::string name, Weapon &type);
+        ~HumanA();
+
+        void attack() const;
+
+    private:
+        std::string _name;
+        Weapon &_type;
+};
+
+#endif
